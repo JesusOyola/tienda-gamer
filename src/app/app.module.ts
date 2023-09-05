b62f7d1cf9944ea10afdb991a8dd9fc402eb9484
JesusOyola <jesus.oyola@interatica.com>
2023-09-05 12:19:04 -0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+
 import { CheckoutComponent } from './pages/components/checkout/checkout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/components/home/home.component';
 
 @NgModule({
   declarations: [
